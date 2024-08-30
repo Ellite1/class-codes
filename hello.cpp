a7d75c219022d238c573d7516bdb88877e346b46
Ellite Baptiste <ebaptiste0@saddleback.edu>
2024-08-30 02:36:36 +0000

    a.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std; 
+int main() {
+   cout << "Hello C++ World!";
+   // that means to ignore
+   /* lkkjl
+   */
+  cout << " We have strarted to learn about coding";
+}
